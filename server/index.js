c506653ed8f4d44b49c321c3b5b8005113cccbd0
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-05-26 18:00:18 +0800

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,29 @@
+  const express = require("express");
+  const cors = require("cors");
+  const dotenv = require("dotenv");
+  const connectDB = require("./config/db");
+  const authroute = require("./routes/authroute");
+  const path = require("path"); 
+  dotenv.config();
+
+
+  // Middleware to handle JSON requests
+  const app = express();
+  app.use(express.json()); 
+  app.use(cors());
+  connectDB();
+  const PORT = process.env.PORT || 8787;
+  app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+  app.use('/auth', authroute);
+
+
+
+
+
+  (async () => {
+    await connectDB(); 
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })();
+
+
+

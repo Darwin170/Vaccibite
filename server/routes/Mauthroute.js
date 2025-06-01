00334192f2cff023451421eb2ed357589b417aaa
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-06-02 04:55:24 +0800

    Update Mauthroute.js

--- server/routes/Mauthroute.js
+++ server/routes/Mauthroute.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const multer = require('multer');
 const { loginUser } = require("../App_controller/userlogin"); 
-const {signupUser} = require ("../App_controller/Signup")
+const {signupUser} = require ("../App_controller/Signup");
+const {addAnimalBite} = require("../App_controller/reportsanimalbite");
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
 const Mrouter = express.Router();
 
 Mrouter.post('/login', loginUser);
 Mrouter.post ('/signup', signupUser);
-
+Mrouter.post('/a', upload.single('file'), addAnimalBite);
 
 module.exports = Mrouter;

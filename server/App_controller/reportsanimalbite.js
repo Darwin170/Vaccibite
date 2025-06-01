ed5a60c895b0e4a9c39851a01867507f9704368e
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-06-02 04:54:05 +0800

    Add files via upload

---
+++ server/App_controller/reportsanimalbite.js
@@ -0,0 +1,49 @@
+const multer = require('multer');
+const Report = require('../model/reportsmodel');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+const addAnimalBite = async (req, res) => {
+  try {
+    const {
+      Name,
+      barangayId, 
+      animalType,
+      color,
+      size,
+      location,
+      severity,
+      caughtStatus
+    } = req.body;
+
+    const filePath = req.file ? req.file.originalname : null;
+
+    const newReport = new Report({
+      type: 'Animal Bite',
+      barangayId, 
+      date: new Date(),
+      status: 'Pending',
+      filePath,
+      categoryDetails: {
+        Name,
+        animalType,
+        color,
+        size,
+        location,
+        severity,
+        caughtStatus
+      }
+    });
+
+    await newReport.save();
+    res.status(201).json({ message: 'Animal Bite reported successfully', report: newReport });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to report Animal Bite', details: error.message });
+  }
+};
+
+module.exports = {
+  upload,
+  addAnimalBite
+};

257f5432c6e0ebe0fb1b4717f3c77072dd4fe63f
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-06-03 16:50:33 +0800

    Update Mauthroute.js

--- server/routes/Mauthroute.js
+++ server/routes/Mauthroute.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const multer = require('multer');
+
 const { loginUser } = require("../App_controller/userlogin"); 
-const {signupUser} = require ("../App_controller/Signup");
-const {addAnimalBite} = require("../App_controller/reportsanimalbite");
-const {addMissinganimal} = require("../App_controller/reportmissinganimal");
-const {addRoamingAnimal} = require("../App_controller/reportroaminganimal");
+const { signupUser } = require("../App_controller/Signup");
+const { addAnimalBite } = require("../App_controller/reportsanimalbite");
+const { addMissinganimal } = require("../App_controller/reportmissinganimal");
+const { addRoamingAnimal } = require("../App_controller/reportroaminganimal");
+
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
+
 const Mrouter = express.Router();
 
-Mrouter.post('/missing', upload.single('file'), addMissinganimal);
-Mrouter.post('/Roaming', upload.single('file'), addRoamingAnimal);
+// Use 'image' consistently as the field name for file uploads
+Mrouter.post('/missing', upload.single('image'), addMissinganimal);
+Mrouter.post('/Roaming', upload.single('image'), addRoamingAnimal);
+Mrouter.post('/a', upload.single('image'), addAnimalBite);
+
+// Non-file routes
 Mrouter.post('/login', loginUser);
-Mrouter.post ('/signup', signupUser);
-Mrouter.post('/a', upload.single('file'), addAnimalBite);
+Mrouter.post('/signup', signupUser);
 
 module.exports = Mrouter;

d1cc7d2d425e27dc26f13b23f026a74f11671ebb
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-08-11 22:02:22 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,51 +1,59 @@
   const express = require("express");
-  const cors = require('cors');
+  const cors = require("cors");
   const dotenv = require("dotenv");
   const connectDB = require("./config/db");
   const authroute = require("./routes/authroute");
   const Mauthroute = require("./routes/Mauthroute");
-  const path = require("path"); 
+  const path = require('path');
+  const nodemailer = require("nodemailer");
+  const bodyParser = require("body-parser");
+  const session = require("express-session");
   dotenv.config();
 
 
   // Middleware to handle JSON requests
   const app = express();
   app.use(express.json()); 
-  const allowedOrigins = [
-  'https://vaccibite.onrender.com', 
-  'http://localhost:8000',         
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
  
-];
 
-app.use(cors({
-  origin: function (origin, callback) {
-   
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+        app.use(session({
+        secret: "supersecretkey",
+        resave: false,
+        saveUninitialized: true,
+      }));
 
-
+      // Configure Nodemailer transporter
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+  
   const PORT = process.env.PORT || 8787;
+ 
   app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
   app.use('/auth', authroute);
   app.use('/mauth',Mauthroute);
 
-app.get('/', (req, res) => {
-  res.send('Backend is running 🚀');
-});
 
 
 
-connectDB();
+
   (async () => {
     await connectDB(); 
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(
+      
+    ));
   })();
+
+
+
 
 
 

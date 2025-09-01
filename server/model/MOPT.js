18d0058b2d48af1729ea6e1b155488c31e8cf60f
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-01 23:51:54 +0800

    Update MOPT.js

--- server/model/MOPT.js
+++ server/model/MOPT.js
@@ -1,1 +1,20 @@
+const mongoose = require("mongoose");
 
+const otpSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "M_User",
+    required: true
+  },
+  
+  otp: { // must match the field used in loginUser & verifyOTP
+    type: Number,
+    required: true
+  },
+  expiresAt: {
+    type: Date,
+    required: true
+  }
+}, { timestamps: true }); // optional: keeps createdAt and updatedAt
+
+module.exports = mongoose.model("OTP", otpSchema);

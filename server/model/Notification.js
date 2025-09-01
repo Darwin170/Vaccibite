630ea2531c6d31e74bdece4b1b9bf5bc912bae6c
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-02 03:52:57 +0800

    Update Notification.js

--- server/model/Notification.js
+++ server/model/Notification.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const notificationSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId,
-     ref: "M_User",
+     ref: "User",
      required: true },
   title: { type: String, required: true },
   message: { type: String, required: true },

284bfe3fe279d08d36f6ed2be3d7f54c08d7521e
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-01 23:42:59 +0800

    Update authcontroller.js

--- server/controller/authcontroller.js
+++ server/controller/authcontroller.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../model/usermode");
 const OTP = require("../model/OPT");
-const  M_Use = require("../model/ M_Use");
+const  M_Use = require("../model/ M_User");
 
 const verifyOTP = async (req, res) => {
   try {
@@ -74,4 +74,5 @@ const verifyOTP = async (req, res) => {
 };
 
 module.exports = { verifyOTP };
+
 

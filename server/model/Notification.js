dad75d227fb7d2e8b64673278d4de1d1c42b43f3
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-02 10:51:18 +0800

    Update Notification.js

--- server/model/Notification.js
+++ server/model/Notification.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
 const NotificationSchema = new mongoose.Schema({
-  senderId: { type: String, required: true },    // User who created the notification
-  receiverId: { type: String, required: true },  // M_User who receives it
+  senderId: { type: String, required: true },     // User who created the notification
+  receiverId: { type: mongoose.Schema.Types.ObjectId, ref: 'M_User', required: true }, // M_User _id
   title: String,
   message: String,
   read: { type: Boolean, default: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model("Notification", NotificationSchema);
-

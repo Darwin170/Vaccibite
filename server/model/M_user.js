dca518660d9f5c34e5cb0883b7b19199317540f4
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-09 11:15:47 +0800

    Update M_user.js

--- server/model/M_user.js
+++ server/model/M_user.js
@@ -2,48 +2,37 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  userId: { 
+   MuserId: { 
     type: String, 
     required: true 
   },
-  name: {
+  fullName: {
     type: String,
-    required: [true, "Name is required"],
-    minlength: [3, "Name must be at least 3 characters"],
-    maxlength: [50, "Name must be at most 50 characters"]
+    required: true,
+    trim: true
   },
   email: {
     type: String,
-    required: [true, "Email is required"],
+    required: true,
     unique: true,
     lowercase: true,
     trim: true,
     match: [/^\S+@\S+\.\S+$/, "Invalid email format"]
   },
-  phone: {
-    type: String,
-    required: [true, "Phone number is required"],
-    match: [/^\d{8,11}$/, "Phone number must be between 8 to 11 digits"]
-  },
   password: {
     type: String,
-    required: [true, "Password is required"],
-    minlength: [8, "Password must be at least 8 characters"]
+    required: true,
   },
-  position: {
-    type: String,
-    required: [true, "Position is required"]
-  },
-}, { timestamps: true });
+  barangay: {
+    type: mongoose.Schema.Types.ObjectId,  
+    ref: 'Barangay',                      
+    required: true
+  }
 
-// Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+}, {
+  timestamps: true
 });
 
-const User = mongoose.model('UserAccounts', userSchema, 'UserAccounts');
+const M_User = mongoose.model('Mobile_User', userSchema);
 
-module.exports = User;
+module.exports = M_User;

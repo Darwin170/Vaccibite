f6bdd5e048eeced01ebe4f76d92f29733048b720
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-10 03:02:29 +0800

    Update M_user.js

--- server/model/M_user.js
+++ server/model/M_user.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
   },
   barangay: {
     type: mongoose.Schema.Types.ObjectId,  
-    ref: 'Barangays',                      
+    ref: 'Barangay',                      
     required: true
   }
 
@@ -36,4 +36,5 @@ const userSchema = new mongoose.Schema({
 const M_User = mongoose.model('Mobile_User', userSchema);
 
 module.exports = M_User;
+
 

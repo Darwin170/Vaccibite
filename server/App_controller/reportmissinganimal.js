92d2562afa5ff36a6878a1a6c12ce05c135b6f7c
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-15 07:51:59 +0800

    Update reportmissinganimal.js

--- server/App_controller/reportmissinganimal.js
+++ server/App_controller/reportmissinganimal.js
@@ -2,31 +2,6 @@ const multer = require('multer');
 const path = require('path');
 const Report = require('../model/reportsmodel');
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-
-
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB max
-  fileFilter: (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|pdf|doc|docx/;
-    const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = allowedTypes.test(file.mimetype);
-
-    if (extname && mimetype) {
-      cb(null, true);
-    } else {
-      cb(new Error('Only JPG, PNG, PDF, DOC, DOCX files are allowed!'));
-    }
-  }
-});
 
 
 const addMissinganimal = async (req, res) => {
@@ -49,6 +24,7 @@ const addMissinganimal = async (req, res) => {
 
     const newReport = new Report({
       type: 'Missing Animal',
+      reportId,
       barangayId, 
       date: new Date(),
       status: 'Pending',
@@ -69,7 +45,7 @@ const addMissinganimal = async (req, res) => {
     await newReport.save();
     res.status(201).json({ message: 'Missing Animal reported successfully', report: newReport });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to report Animal Bite'});
+    res.status(500).json({ error: 'Failed to report Missing Animal'});
   }
 };
 
@@ -77,4 +53,5 @@ module.exports = {
   upload,
   addMissinganimal
 };
+
 

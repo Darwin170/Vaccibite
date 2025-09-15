65742db40044a88119d1fd9d118250cd444d2359
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-15 18:48:32 +0800

    Update Activitylogs.js

--- server/model/Activitylogs.js
+++ server/model/Activitylogs.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
-require("../model/usermode"); 
+// Make sure both user models are required so Mongoose knows about them
+require("../model/usermode");   // Your UserAccounts model
+require("../model/M_user");     // Your M_User model
 
 const activityLog = new mongoose.Schema({
+  // This 'user' field will hold the ObjectId of a user from either collection.
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "UserAccounts", 
     required: true,
+    refPath: 'onModel' // This is the key that makes it dynamic
+  },
+  onModel: {
+    type: String,
+    required: true,
+    enum: ['UserAccounts', 'Mobile_User'] // Only these values are allowed
   },
   action: {
     type: String,

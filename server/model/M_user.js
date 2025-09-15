20364b861d6a5c8c4f434cd9440d026898131178
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-15 17:42:04 +0800

    Update M_user.js

--- server/model/M_user.js
+++ server/model/M_user.js
@@ -2,40 +2,42 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-   MuserId: { 
-    type: String, 
-    required: true 
-  },
-  fullName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    match: [/^\S+@\S+\.\S+$/, "Invalid email format"]
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  barangay: {
-    type: mongoose.Schema.Types.ObjectId,  
-    ref: 'Barangays',                      
-    required: true
-  }
-
+    MuserId: {  
+        type: String,  
+        required: true  
+    },
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, "Invalid email format"]
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    barangay: {
+        type: mongoose.Schema.Types.ObjectId,              
+        ref: 'Barangays',                          
+        required: true
+    },
+    // This field automatically sets the role to 'mobile'
+    position: {
+        type: String,
+        enum: ['mobile'], // Ensures the only possible role is 'mobile'
+        default: 'mobile',
+    }
 }, {
-  timestamps: true
+    timestamps: true
 });
 
 const M_User = mongoose.model('Mobile_User', userSchema);
 
 module.exports = M_User;
-
-
-

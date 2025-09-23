b0ad8654464f8988e4ccf507264016c0d324d1fa
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-23 13:58:13 +0800

    Update index.js

--- client/src/Pages/System_Admin/index.js
+++ client/src/Pages/System_Admin/index.js
@@ -4,7 +4,7 @@ import Activitylogs from '../System_Admin/Activitylogs';
 import UserManagement from '../System_Admin/UserManagement';
 import Backup from '../System_Admin/backup';
 import Archive from '../System_Admin/Archiving';
-
+import MobileUser from '../System_Admin/MobileUser'
 
 export default function System_AdminRoutes() {
   return (
@@ -13,6 +13,7 @@ export default function System_AdminRoutes() {
       <Route path="UserManagement" element={<UserManagement />} />
       <Route path="Backup" element={<Backup />} />
       <Route path="Archive" element={<Archive />} />
+      <Route path="MobileUser"element={<MobileUser/>}/>
     </Routes>
   );
 }

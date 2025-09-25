ca688f61cf6b05d3a33de4ffe800abe689291163
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-25 15:19:30 +0800

    Update resend-OTP.js

--- server/controller/resend-OTP.js
+++ server/controller/resend-OTP.js
@@ -4,7 +4,9 @@ const nodemailer = require("nodemailer");
 
 // Gmail transporter
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // Must be 'true' for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,

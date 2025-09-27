871fad6ea1ec043babdf07fcffa0abaa86bdbbe7
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-27 14:37:00 +0800

    Update Notification.js

--- server/model/Notification.js
+++ server/model/Notification.js
@@ -11,11 +11,17 @@ const NotificationSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Reports"
   },
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Assuming Admins are also in the M_User collection
+    required: false // It might be null if the notification is system-generated
+  },
   title: { type: String, required: true },
   message: { type: String, required: true },
   read: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model("Notification", NotificationSchema);
+
 
 

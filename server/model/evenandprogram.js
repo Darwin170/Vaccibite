844f8909855ad63e58b8feb6ff159982d0149dc1
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-28 13:57:55 +0800

    Update evenandprogram.js

--- server/model/evenandprogram.js
+++ server/model/evenandprogram.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
 const EventSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  start: { type: Date, required: true },
-  end: { type: Date, required: true },
-  details: { type: String, default: '' },
-  barangayId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Barangays',
-    required: true
-  },
-  views: { type: Number, default: 0 }  // 👈 NEW FIELD
+    title: { type: String, required: true },
+    start: { type: Date, required: true },
+    end: { type: Date, required: true },
+    details: { type: String, default: '' },
+    barangayId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Barangays',
+        required: true
+    },
+    viewedByMobileUsers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Mobile_User' 
+    }]
 });
 
 module.exports = mongoose.model('Event', EventSchema);

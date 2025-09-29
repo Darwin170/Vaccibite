3589e041e885f4c1e6d15febbf84d5240d8b7a66
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-29 17:00:05 +0800

    Update PrivateRoute.js

--- client/src/routes/PrivateRoute.js
+++ client/src/routes/PrivateRoute.js
@@ -1,23 +1,24 @@
-// src/routes/PrivateRoute.js
+
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const PrivateRoute = ({ children, role }) => {
-  const { user } = useAuth(); // get from AuthContext
+  const { user } = useAuth(); 
 
-  // If no session → redirect to login
+  
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
-  // If wrong role → redirect
+ 
   if (role && user.role !== role) {
     return <Navigate to="/login" replace />;
   }
 
-  // ✅ Authenticated + authorized
+
   return children;
 };
 
 export default PrivateRoute;
+

1853cb1a269942cd8b426f02bf76c79bdf7af69d
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-09-29 18:32:18 +0800

    Create recordEventPageClick.js

---
+++ server/App_controller/recordEventPageClick.js
@@ -0,0 +1,32 @@
+const Event = require('../model/evenandprogram'); 
+const ClickLog = require('../model/EventPageClickLog');
+
+const recordEventPageClick = async (req, res) => {
+    try {
+        const mobileUserId = req.user._id;
+
+        if (!mobileUserId) {
+            return res.status(401).json({ message: 'Mobile user not authenticated.' });
+        }
+
+        // 💡 Use the new ClickLog model here
+        const logEntry = new ClickLog({
+            userId: mobileUserId,
+        });
+
+        await logEntry.save();
+
+        res.status(201).json({ 
+            message: 'Event page entry tracked successfully.',
+            logId: logEntry._id
+        });
+
+    } catch (err) {
+        console.error("Error recording event page click:", err);
+        res.status(500).json({ message: 'Server error occurred while tracking page click.' });
+    }
+};
+
+module.exports = {  
+    recordEventPageClick
+};

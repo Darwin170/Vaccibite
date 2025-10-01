67e396850fd02ad3f6696104e9978221a27538f6
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-10-02 00:10:50 +0800

    Update M_user.js

--- server/model/M_user.js
+++ server/model/M_user.js
@@ -31,7 +31,7 @@ const userSchema = new mongoose.Schema({
    position: {
         type: String,
         enum: ['Barangay Presentative'], // Ensures the only possible role is 'mobile'
-        default: 'mobile',
+        default: 'Barangay Presentative',
     }
 
 
@@ -42,6 +42,7 @@ const userSchema = new mongoose.Schema({
 const M_User = mongoose.model('Mobile_User', userSchema);
 
 module.exports = M_User;
+
 
 
 

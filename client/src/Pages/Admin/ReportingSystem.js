3f8e7b40ee5ad2a725065acadb41463f2e6d356f
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2025-10-06 23:18:33 +0800

    Update ReportingSystem.js

--- client/src/Pages/Admin/ReportingSystem.js
+++ client/src/Pages/Admin/ReportingSystem.js
@@ -327,7 +327,7 @@ const updateReportStatus = async (reportId, newStatus, file = null) => {
                                 <p>
                                     <strong>File:</strong>{' '}
                                     <a
-                                        href={`${API_URL}/${selectedDetails.filePath}`}
+                                        href={`${process.env.REACT_APP_API_URL}/${report.filePath}`}
                                         download
                                         target="_blank"
                                         rel="noopener noreferrer"
@@ -492,6 +492,7 @@ const updateReportStatus = async (reportId, newStatus, file = null) => {
 }
 
 export default ReportingPage;
+
 
 
 

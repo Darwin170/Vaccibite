c408e271753716b2060b5970ca21cba54b8da21f
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2026-02-06 10:07:40 +0800

    Update MobileUser.js

--- client/src/Pages/Superadmin/MobileUser.js
+++ client/src/Pages/Superadmin/MobileUser.js
@@ -94,7 +94,7 @@ function M_user() {
     try {
 
       await axios.put(
-        `${process.env.REACT_APP_API_URL}/auth/activecontroller/${user._id}`,
+        `${process.env.REACT_APP_API_URL}/auth/toggleMUserStatus/${user._id}`,
         { isActive: newStatus },
         {
           headers: {
@@ -266,6 +266,7 @@ function M_user() {
 }
 
 export default M_user;
+
 
 
 

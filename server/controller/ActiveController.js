75e6380cc1865fa5c518fd61d21a062201950dae
Umengan Darwin <111472882+Darwin170@users.noreply.github.com>
2026-03-09 00:04:26 +0800

    Update ActiveController.js

--- server/controller/ActiveController.js
+++ server/controller/ActiveController.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.GMAIL_USER, 
-        pass: process.env.GMAIL_APP_PASSWORD, 
+      user: process.env.EMAIL_USER,  
+        pass: process.env.EMAIL_PASS, 
     },
 });
 
